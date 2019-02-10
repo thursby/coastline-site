baf7dfb4b78ad6e004b6c98ca4386f5a2053ee98
Wayne Thursby <wayne.thursby@1srq.com>
2019-02-10 18:01:52 +0000

    Updated to current production photos.

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,26 @@
-const { DateTime } = require("luxon");
-
 module.exports = function(eleventyConfig) {
-  // Add a filter using the Config API
-  eleventyConfig.addFilter( "myFilter", function() {});
-  eleventyConfig.addPassthroughCopy("./src/_redirects");
+  // Define files to pass through without modification
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
+  eleventyConfig.addPassthroughCopy("src/.htaccess");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/humans.txt");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+  eleventyConfig.addPassthroughCopy("src/browserconfig.xml");
+  eleventyConfig.addPassthroughCopy("src/404.html");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
   
-
-    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
-  });
-  
-  // You can return your Config object (optional).
+  // Custom config settings
   return {
     passthroughFileCopy: true,
     dir: {
       input: "src",
       output: "dist"
     },
-    templateFormats: ["md","liquid","njk","html","jpg","png","css","js","woff","htaccess","txt","webmanifest","ico","xml"],
+    templateFormats: ["md","liquid","njk"],
     pathPrefix: '/'
   };
 };
